08adea4dda9273c7cc121663512e3cd2c9f99cf7
CarlosEduardoLemosNascimento <159453764+CarlosEduardoLemosNascimento@users.noreply.github.com>
2024-09-11 01:44:36 +0000

    ,

--- meu_projeto/main.py
+++ meu_projeto/main.py
@@ -2,7 +2,7 @@ import os
 
 from datetime import date
 from models.enums.sexo import Sexo
-from models.enums.unidade_federativa import UnidadeFederativa
+from models.enums.unidadeFederativa import UnidadeFederativa
 from models.endereco import Endereco
 from models.pessoa import Pessoa
 
@@ -14,4 +14,5 @@ endereco = Endereco("Rua das Flores", "123", "Apto 45", "12345-678", "Salvador",
 pessoa = Pessoa(1, "João Silva", date(1990, 5, 21), "9999-9999", "joao.silva@email.com", Sexo.MASCULINO, endereco)
 
 # Exibindo informações da pessoa
+os.system("cls || clear")
 print(pessoa)
--- meu_projeto/models/endereco.py
+++ meu_projeto/models/endereco.py
@@ -1,4 +1,4 @@
-from models.enums.unidade_federativa import UnidadeFederativa
+from models.enums.unidadeFederativa import UnidadeFederativa
 
 class Endereco:
     def __init__(self, logradouro: str, numero: str, complemento: str, cep: str, cidade: str, uf: UnidadeFederativa):
